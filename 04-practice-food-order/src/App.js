fe4fac814323d15a39f030851afa183a0abbfeec
moamen2020 <m01144944981@gmail.com>
2022-05-28 01:50:39 +0200

    Project => food order

---
+++ 04-practice-food-order/src/App.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Cart from "./component/Cart/cart";
+import Header from "./component/Layout/Header";
+import Meals from "./component/Meals/Meals";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShow(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  };
+  return (
+    <CartProvider>
+      {cartIsShow && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CartProvider>
+  );
+}
+
+export default App;
