1c0171247d8c056895e052701adc0cb20ac39a63
moamen2020 <m01144944981@gmail.com>
2022-06-05 08:41:16 +0200

    finsh project

--- 07-dates-reminder/src/App.js
+++ 07-dates-reminder/src/App.js
@@ -1,30 +1,29 @@
-import { Col, Container, Row } from "react-bootstrap";
-
-
+import { Button, Col, Container, Row } from "react-bootstrap";
+import React, { useState, useEffect } from 'react'
+import { person } from './data'
+import DatesCount from './components/DatesCount'
+import DatesList from './components/DatesList'
+import DatesAction from './components/DatesAction'
 function App() {
+
+  const [personData, setPersonData] = useState(person)
+  //dafadf
+  const onDelete = () => {
+    setPersonData([])
+  }
+  const onViewData = () => {
+    setPersonData(person)
+  }
+  useEffect(() => {
+    setPersonData([])
+  }, [])
   return (
     <div className="font color-body">
-      <Container className="py-4">
-
-        <Row className="justify-content-center my-2">
-          <Col sm='8'>لديك 4 مواعيد اليوم</Col>
-        </Row>
-
-        <Row className="justify-content-center">
-          <Col sm='8'>
-            <div className="rectangle"></div>
-          </Col>
-        </Row>
-
-
-        <Row className="justify-content-center my-2" >
-          <Col sm='8' className="d-flex justify-content-between">
-            <button className="btn-style p-2"> حذف الكل</button>
-            <button className="btn-style p-2">عرض البيانات</button>
-          </Col>
-        </Row>
+      <Container className="py-5">
+        <DatesCount person={personData} />
+        <DatesList person={personData} />
+        <DatesAction deleteData={onDelete} onViewData={onViewData} />
       </Container>
-      
     </div>
   );
 }
