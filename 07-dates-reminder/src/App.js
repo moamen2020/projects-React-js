ae4743230e33620418d19004d92db869eacf9376
moamen2020 <m01144944981@gmail.com>
2022-06-05 06:28:42 +0200

    Building Design project

--- 07-dates-reminder/src/App.js
+++ 07-dates-reminder/src/App.js
@@ -1,10 +1,30 @@
-import { Button } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 
 function App() {
   return (
-    <div className="m">
-      <Button>Welcome</Button>
+    <div className="font color-body">
+      <Container className="py-4">
+
+        <Row className="justify-content-center my-2">
+          <Col sm='8'>لديك 4 مواعيد اليوم</Col>
+        </Row>
+
+        <Row className="justify-content-center">
+          <Col sm='8'>
+            <div className="rectangle"></div>
+          </Col>
+        </Row>
+
+
+        <Row className="justify-content-center my-2" >
+          <Col sm='8' className="d-flex justify-content-between">
+            <button className="btn-style p-2"> حذف الكل</button>
+            <button className="btn-style p-2">عرض البيانات</button>
+          </Col>
+        </Row>
+      </Container>
+      
     </div>
   );
 }
